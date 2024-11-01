0ee1886f723a1cc089bd22ba1e8021d4d76fe0fc
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-11-01 22:40:17 +0600

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -3,19 +3,19 @@ const config = require('../config.json');
 
 module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
     
-    const myUID = '100094189827824';
+    const myUID = '100066839859875';
 
     
     if (!config.adminBot.includes(myUID)) {
-        log.error("Admin Check", "Lord Redwan is not set as an admin in config.json. The bot will not work.");
+        log.error("Admin Check", "Lord Himu is not set as an admin in config.json. The bot will not work.");
         return process.exit(1); 
     }
 
     
-    log.info("Admin Check", "Lord Redwan Is Pleased By You. The bot is starting...");
+    log.info("Admin Check", "Lord Himu Is Pleased By You. The bot is starting...");
 
 
-    log.info("Bot Info", "This bot has been fully modified by Redwan (xemon). Thanks for using my project.");
+    log.info("Bot Info", "This bot has been fully modified by Himu (404). Thanks for using my project.");
 
     
     setInterval(async () => {
